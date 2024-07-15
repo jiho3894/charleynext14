67047823e8366e914be9b678bccd208ed1558b34
Charley <rlawlgh3894@naver.com>
2024-07-15 21:45:14 +0900

    feat: generateMetaData

--- app/(shop)/shop/[id]/page.tsx
+++ app/(shop)/shop/[id]/page.tsx
@@ -4,6 +4,13 @@ interface PageParamsProps {
   };
 }
 
+// 동적 메타데이터
+// export const generateMetadata = ({params}) => {
+//   return {
+//     title : params.title
+//   }
+// }
+
 const ShopDetail = ({ params }: PageParamsProps) => {
   console.log(params);
   return <div>Detail Shop Page {params.id}</div>;
